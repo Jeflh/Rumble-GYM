950ce2636665e68f812feae95a7fc66e85ba44c7
Emmanuel Fernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-18 16:39:31 -0600

    Se estableció un nav y un footer globales.

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,13 @@
-<footer class="bg-primary text-center text-lg-start pt-2">
+<footer class="bg-dark text-center text-lg-start pt-2">
 
   <div class="text-center m-3">
-    <a class="text-light" href="#"></a>
-    <p class="text-light mb-0">Todos los derechos reservados - Rumble GYM <?php echo date('Y'); ?> &copy;</p>
+    <a href="#"></a>
+    <p class="mb-0">Todos los derechos reservados - Rumble GYM <?php echo date('Y'); ?> &copy;</p>
   </div>
 
 </footer>
 
-<script src="/src/js/bootstrap.bundle.min.js"></script>
+<script src="src/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
