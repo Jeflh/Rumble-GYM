9db7d38b64c1bc647c9ccad0b6da5c185851b26a
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-19 15:49:06 -0600

    Funciones para ambos login listas

--- controllers/Login.php
+++ controllers/Login.php
@@ -3,7 +3,11 @@
 class LoginController{
 
   public function index(){
-    require_once('views/login/V_login.php');
+    require_once('views/login/V_loginClientes.php');
+  }
+
+  public function empleados(){
+    require_once('views/login/V_loginEmpleados.php');
   }
 }
 
