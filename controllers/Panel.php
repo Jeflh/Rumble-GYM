f9888af23d32792eb9f75c5ab1833afb1ce1e95e
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-20 23:21:13 -0600

    Condiciones para vista de panel establecidas.

--- controllers/Panel.php
+++ controllers/Panel.php
@@ -28,9 +28,6 @@ class PanelController{
     else if(isset($_SESSION['usuario']['estado'])) {
       require_once('views/panel/V_panelCliente.php');
     }
-    else{
-      header("Location: index.php?c=login");
-    }
   }
 }
 
