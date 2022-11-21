7d553b29ca5f42e1158b2fde33197cf8b59e8205
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-21 02:11:28 -0600

    Cambio en nombre de archivo.

--- views/empleado/V_inicioEmpleados.php
+++ views/empleado/V_inicioEmpleados.php
@@ -21,19 +21,19 @@ require_once 'includes/navLogueado.php';
         if ($status == '0') {
           echo '<div class="text-center alert alert-dismissible alert-success mb-2">
           <button type="button" class="btn-close " data-bs-dismiss="alert"></button>
-          <strong>Producto añadido</strong>, el producto ha sido añadido correctamente.
+          <strong>Empleado registrado</strong>, el empleado ha sido registrado correctamente.
           </div>';
         }
         if ($status == '1') {
           echo '<div class="text-center alert alert-dismissible alert-success mb-2">
           <button type="button" class="btn-close " data-bs-dismiss="alert"></button>
-          <strong>Producto actualizado</strong>, el producto ha sido actualizado correctamente.
+          <strong>Empleado actualizado</strong>, el empleado ha sido actualizado correctamente.
           </div>';
         }
         if ($status == '2') {
           echo '<div class="text-center alert alert-dismissible alert-success mb-2">
           <button type="button" class="btn-close " data-bs-dismiss="alert"></button>
-          <strong>Producto eliminado</strong>, el producto ha sido eliminado correctamente.
+          <strong>Empleado eliminado</strong>, el empleado ha sido eliminado correctamente.
           </div>';
         }
       }
@@ -51,22 +51,32 @@ require_once 'includes/navLogueado.php';
         </tr>
       </thead>
       <tbody>
-        <?php foreach ($productos as $producto) : ?>
+        <?php foreach ($empleados as $empleado) : ?>
           <tr class="table-default">
-            <th scope="row"><?php echo $producto['id_producto']; ?></th>
-            <td><?php echo $producto['nombre']; ?></td>
-            <td><?php echo $producto['descripcion']; ?></td>
-            <td><?php echo $producto['cantidad']; ?></td>
-            <td><?php echo '$' . $producto['precio']; ?></td>
+            <th scope="row"><?php echo $empleado['id_empleado']; ?></th>
+            <td><?php echo $empleado['nombre'] . ' ' . $empleado['apellido_paterno'] . ' ' . $empleado['apellido_materno']; ?></td>
+            <td><?php echo date('d/m/Y', strtotime($empleado['fecha_nacimiento'])); ?></td>
+            <td><?php echo $empleado['domicilio']; ?></td>
+            <td><?php echo $empleado['telefono']; ?></td>
             <td>
-   
-              <a class="btn btn-primary btn-sm me-2 text-light" href="index.php?c=producto&a=editar&id=<?php echo $producto['id_producto']; ?>">
+              <?php 
+                if($empleado['tipo'] == '1'){
+                  echo 'Administrador';
+                } else if($empleado['tipo'] == '2'){
+                  echo 'Recepcionista';
+                } else {
+                  echo 'Entrenador';
+                }
+              ?>
+            </td>
+            <td>
+              <a class="btn btn-primary btn-sm me-2 text-light" href="index.php?c=empleado&a=editar&id=<?php echo $empleado['id_empleado']; ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                 </svg>
               </a>
       
-              <a class="btn btn-warning btn-sm text-light" href="index.php?c=producto&a=eliminar&id=<?php echo $producto['id_producto']; ?>">
+              <a class="btn btn-warning btn-sm text-light" href="index.php?c=empleado&a=eliminar&id=<?php echo $empleado['id_empleado']; ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                   <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                 </svg>
