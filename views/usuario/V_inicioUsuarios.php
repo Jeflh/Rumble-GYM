3181ddda5c6b23a697149da3d6cddb2f4f3f59fa
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-23 12:48:52 -0600

    Vistas para la gestión de usuarios terminadas.

--- views/usuario/V_inicioUsuarios.php
+++ views/usuario/V_inicioUsuarios.php
@@ -49,12 +49,11 @@ require_once 'includes/navLogueado.php';
         <tr>
           <th scope="col" class="col">ID</th>
           <th scope="col" class="col">Estado</th>
-          <th scope="col" class="col-3">Nombre</th>
-          <th scope="col" class="col">Teléfono</th>
+          <th scope="col" class="col-4">Nombre</th>
           <th scope="col" class="col">Suscripción</th>
           <th scope="col" class="col">Tipo</th>
           <th scope="col" class="col">Vence</th>
-          <th scope="col" class="col">Acciones</th>
+          <th scope="col" class="col-2">Acciones</th>
         </tr>
       </thead>
       <tbody>
@@ -70,7 +69,6 @@ require_once 'includes/navLogueado.php';
                 ?>
             </td>
             <td><?php echo $usuario['nombre'] . ' ' . $usuario['apellido_p'] . ' ' . $usuario['apellido_m']; ?></td>
-            <td><?php echo $usuario['telefono']; ?></td>
             <td><?php
                 $status = fechaEnRango($usuario['fecha_inicio'], $usuario['fecha_fin']);
                 if ($status == true) {
@@ -95,7 +93,7 @@ require_once 'includes/navLogueado.php';
             <td><?php echo date("d-m-Y", strtotime($usuario['fecha_fin'])); ?></td>
 
             <td>
-              <a class="btn btn-primary btn-sm me-1 text-light" href="index.php?c=usuario&a=editar&id=<?php echo $usuario['id_usuario']; ?>">
+              <a class="btn btn-light btn-sm me-1" href="index.php?c=usuario&a=renovar&id=<?php echo $usuario['id_usuario']; ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
                   <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
@@ -103,19 +101,19 @@ require_once 'includes/navLogueado.php';
               </a>
 
               <a class="btn btn-primary btn-sm me-1 text-light" href="index.php?c=usuario&a=editar&id=<?php echo $usuario['id_usuario']; ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                 </svg>
               </a>
 
-              <a class="btn btn-primary btn-sm me-1 text-light" href="index.php?c=usuario&a=editar&id=<?php echo $usuario['id_usuario']; ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+              <a class="btn btn-danger btn-sm me-1 text-light" href="index.php?c=usuario&a=estado&id=<?php echo $usuario['id_usuario']; ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z" />
                 </svg>
               </a>
 
               <a class="btn btn-warning btn-sm text-light" href="index.php?c=usuario&a=eliminar&id=<?php echo $usuario['id_usuario']; ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                   <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                 </svg>
               </a>
