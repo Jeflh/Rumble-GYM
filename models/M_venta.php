859eab962420f463176f9f3ded1d5cdb37e24ee3
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-24 01:38:36 -0600

    Controlador de ventas terminado.

--- models/M_venta.php
+++ models/M_venta.php
@@ -3,6 +3,7 @@
 class VentaModel{
   private $db;
 
+  private $id_empleado;
   private $fecha_venta;
   private $monto_venta;
 
@@ -14,6 +15,20 @@ class VentaModel{
   }
 
   
+  public function insertVenta($datos){
+    $this->id_empleado = mysqli_real_escape_string($this->db, $datos['id_empleado']);
+    $this->fecha_venta = mysqli_real_escape_string($this->db, $datos['fecha_venta']);
+    $this->monto_venta = mysqli_real_escape_string($this->db, $datos['monto_venta']);
+
+    $query = $this->db->query("INSERT INTO ventas (id_empleado, fecha_venta, monto_venta) VALUES ('$this->id_empleado', '$this->fecha_venta', '$this->monto_venta')");
+
+    if($query){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
 }
 
 ?>
