01d10879fc7320e25074cddb54e7bd113c1bb708
Juan Emmanuel Fernández de Lara Hernández <Juan.FernandezDeLara2865@alumnos.udg.mx>
2022-11-27 22:47:30 -0600

    correción en renovar suscripción

--- views/usuario/V_usuario.php
+++ views/usuario/V_usuario.php
@@ -6,14 +6,24 @@ require_once 'includes/navLogueado.php';
 <main>
   <div class="container">
     <h1 class="text-light text-center mt-2"><strong><?php echo  $usuario['id_usuario'] . ' - ' . $usuario['nombre'] . ' ' . $usuario['apellido_p'] . ' ' . $usuario['apellido_m']; ?></strong></h1>
-  
+
     <div class="d-flex justify-content-between mb-3">
       <a href="index.php?c=usuario" class="btn btn-info">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z" />
         </svg>
       </a>
-      <a class="btn btn-light me-1" href="index.php?c=usuario&a=renovar&id=<?php echo $usuario['id_usuario']; ?>">
+      <?php if ($usuario['tipo_suscripcion'] == '1') {
+        $costo = 300;
+      } else if ($usuario['tipo_suscripcion'] == '2') {
+        $costo = 855;
+      } else if ($usuario['tipo_suscripcion'] == '3') {
+        $costo = 1620;
+      } else if ($usuario['tipo_suscripcion'] == '4') {
+        $costo = 2880;
+      }
+      ?>
+      <a class="btn btn-light me-1" href="index.php?c=usuario&a=renovar&id=<?php echo $usuario['id_usuario']  . '&em=' . $_SESSION['usuario']['id_empleado'] . '&m=' . $costo; ?>">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
           <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
@@ -39,7 +49,7 @@ require_once 'includes/navLogueado.php';
       </a>
     </div>
   </div>
-  
+
   <h2 class="text-light text-center mt-2"><strong>Detalles de usuario</strong></h2>
 
   <div class="d-flex justify-content-center text-center">
